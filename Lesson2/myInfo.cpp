399d690dc292b643d224a1cbfc40177b8197ca0d
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-21 01:14:48 +0600

    Printed some text

--- Lesson2/myInfo.cpp
+++ Lesson2/myInfo.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
 int main(){
-    std::cout << "My name is Zoe.\n";
-    std::cout << "I study CSE.\n";
-    std::cout << "I am learning C++.\n";
+    std::cout << "Hello!\n"
+              << "My name is Zoe.\n"
+              << "I study CSE.\n"
+              << "I am learning C++.\n";
+
     return 0;
 }
