fe2574b7b68c19a76707737b87a89c78424fd677
ZobayerAhamed-Anik <zazobayer@gmail.com>
2026-07-21 01:56:19 +0600

    Variable practice for a Student info.

---
+++ Lesson3/studentInfo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+    int age = 20;
+    double cgpa = 3.61;
+    char grade = 'A';
+    bool passed = true;
+
+    std::cout   << "Age: " << age << "\n"
+                << "CGPA: " << cgpa << "\n"
+                << "Grade: " << grade << "\n"
+                << "Passed: " << passed << "\n";
+    return 0;
+}
---
+++ Lesson3/variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int age = 20;
+    int year = 2026;
+    int marks = 95;
+    std::cout << age << "\n";
+    std::cout << year << "\n";
+    std::cout << marks;
+    return 0;
+}
